b10a54db9d7c128e49e2669488d008c952a33313
FranciscoSanQA <franciscosanchezdelrosario@gmail.com>
2022-05-04 15:13:13 +0200

    exercise

--- src/test/java/serenityswag/inventory/WhenAddingAnItemToTheCart.java
+++ src/test/java/serenityswag/inventory/WhenAddingAnItemToTheCart.java
@@ -4,13 +4,16 @@ import net.serenitybdd.core.Serenity;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import serenityswag.authentication.LoginActions;
-import serenityswag.cart.AddToCardActions;
-import serenityswag.cart.ShoppingCartBadge;
+import serenityswag.cart.CardActions;
+import serenityswag.cart.ShoppingCartIcon;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static serenityswag.authentication.User.STANDARD_USER;
@@ -29,29 +32,52 @@ public class WhenAddingAnItemToTheCart {
         login.as(STANDARD_USER);
     }
 
-    ShoppingCartBadge shoppingCartBadge;
+    ShoppingCartIcon shoppingCartBadge;
 
     @Steps
-    AddToCardActions addToCard;
+    CardActions cart;
 
     @Test
     public void theCorrectItemCountShouldBeShown() {
         //Check that the shopping cart badge is empty
         Serenity.reportThat("The shopping cart badge should be empty",
-                ()-> assertThat(shoppingCartBadge.count()).isEmpty()
+                ()-> assertThat(shoppingCartBadge.badgeCount()).isEmpty()
         );
 
         //Add 1 item to the cart
-        addToCard.item("Sauce Labs Backpack");
+        cart.addItem("Sauce Labs Backpack");
 
-        //The shopping cart badge should be one
+        //The shopping cart badge should be 1
         Serenity.reportThat("The shopping cart badge should now be 1",
-                ()->  assertThat(shoppingCartBadge.count()).isEqualTo("1")
+                ()->  assertThat(shoppingCartBadge.badgeCount()).isEqualTo("1")
         );
     }
 
+    ProductList productList;
+    
     @Test
     public void allTheItemsShouldAppearInTheCart() {
+        //Add several item to the cart
+        List<String> selectedItems= firstThreeProductTitlesDisplayed();
+        
+        //Open the cart page
+        cart.adItems(selectedItems);
 
+        String cartBadgeCount= Integer.toString(selectedItems.size());
+        Serenity.reportThat("The shopping cart badge should now be " + cartBadgeCount,
+                ()->  assertThat(shoppingCartBadge.badgeCount()).isEqualTo(cartBadgeCount)
+        );
+
+        cart.openCart();
+
+        //Should see all the item listed
+        Serenity.reportThat("Should see all the items listed " ,
+                ()->         assertThat(cart.displayedItems()).containsExactlyElementsOf(selectedItems)
+        );
+    }
+
+    @NotNull
+    private List<String> firstThreeProductTitlesDisplayed() {
+        return productList.titles().subList(0, 3);
     }
 }
