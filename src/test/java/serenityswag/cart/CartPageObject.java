882d6c8c760da3fc97b94c60aeadca7463b218d9
FranciscoSanQA <franciscosanchezdelrosario@gmail.com>
2022-05-07 20:59:00 +0200

    refactor findby

--- src/test/java/serenityswag/cart/CartPageObject.java
+++ src/test/java/serenityswag/cart/CartPageObject.java
@@ -4,6 +4,8 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.ArrayList;
@@ -12,33 +14,30 @@ import java.util.List;
 @DefaultUrl("https://www.saucedemo.com/cart.html")
 public class CartPageObject extends PageObject {
 
-    @FindBy(id = "checkout")
-    WebElementFacade checkoutButton;
-
     @FindBy(css = ".title")
     WebElementFacade title;
 
+    private static final By CHECKOUT_BUTTON = By.id("checkout");
+    private static final By TITLE = By.cssSelector(".title");
+    private static final By CART_ITEMS = By.cssSelector(".cart_item");
 
-    @FindBy(className = "cart_item")
-    List<WebElementFacade> cartItemElements;
 
     public void checkout() {
-        checkoutButton.click();
+        $(CHECKOUT_BUTTON).click();
     }
 
     public String getTitleText() {
-        return title.getText();
+        return $(TITLE).getText();
     }
 
     public List<CartItem> items() {
-        List<CartItem> cartItems = new ArrayList<>();
-        for (WebElementFacade cartItemElement : cartItemElements) {
-            String name = cartItemElement.findBy(".inventory_item_name").getText();
-            String description = cartItemElement.findBy(".inventory_item_desc").getText();
-            Double price = priceFrom(cartItemElement.findBy(".inventory_item_price").getText());
-            cartItems.add(new CartItem(name, description, price));
-        }
-        return cartItems;
+        return findAll(CART_ITEMS).map(
+                item -> new CartItem(
+                        item.findBy(".inventory_item_name").getText(),
+                        item.findBy(".inventory_item_desc").getText(),
+                        priceFrom(item.findBy(".inventory_item_price").getText())
+                )
+        );
     }
 
     @NotNull
